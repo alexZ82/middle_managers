7201fb6020c5e6bf2a5a1b2fc4f5810904c52b34
alexZ82 <alexia.zoumpoulaki@gmail.com>
2018-08-28 12:20:27 +0100

    commit operation

---
+++ simulation/agent.py
@@ -0,0 +1,9 @@
+import random
+
+class Agent:
+
+  def __init__(self):
+    self.years_in_company = random.randint(1,5)#https://www.ft.com/content/ded1823a-370e-11e7-99bd-13beb0903fa3
+    self.previous_experience = random.randint(1,8)
+    self.belongigness = random.random()	       #THINK
+
